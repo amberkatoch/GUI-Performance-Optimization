cf9387b10e37148774449a2ddba8c72c25370115
Jasleen Kaur <jazzlinkaur02@gmail.com>
2026-06-25 17:04:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const button=document.getElementById("btn");
+
+button.addEventListener("click",()=>{
+
+console.log("Optimization executed.");
+
+alert("GUI Performance Optimized!");
+
+});
+
+window.addEventListener("load",()=>{
+
+console.log("Application Loaded Successfully");
+
+});
